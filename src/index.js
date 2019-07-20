f7baf732fa7c936be1da598f562e5b17512daf24
Gabriel Monteiro Fernandes <gamofe@protonmail.ch>
2019-07-20 20:34:18 -0300

    Módulo 10 - Configurando StatusBar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 import Routes from './routes'
 
 function App() {
-  return <Routes />
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#62b0cc" />
+      <Routes />
+    </>
+  )
 }
 
 export default App
