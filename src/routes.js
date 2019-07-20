0de9bd8a0fe3e7ea3f690d141114c6cce1aa5119
Gabriel Monteiro Fernandes <gamofe@protonmail.ch>
2019-07-20 20:52:15 -0300

    Arrumado nome de pasta

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
-import SignIn from './Pages/SignIn/config'
-import SignUp from './Pages/SignUp/config'
+import SignIn from './pages/SignIn/config'
+import SignUp from './pages/SignUp/config'
 
 export default createAppContainer(
   createSwitchNavigator({
