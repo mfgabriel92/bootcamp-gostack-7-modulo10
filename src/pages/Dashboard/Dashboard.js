32397b73d80a14a29a88ec7ec5de770ab5920401
Gabriel Monteiro Fernandes <gamofe@protonmail.ch>
2019-07-21 18:20:39 -0300

    Módulo 10 - Lista de agendamentos

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Background from '../../componenents/Background'
+import { Container, Title, Appointments } from './styles'
+import Appointment from '../../componenents/Appointment'
 
 function Dashboard() {
   return (
     <Background>
-      <></>
+      <Container>
+        <Title>Schedules</Title>
+
+        <Appointments
+          data={[1, 2, 3, 4, 5]}
+          keyExtractor={item => String(item)}
+          renderItem={({ item }) => <Appointment data={item} />}
+        />
+      </Container>
     </Background>
   )
 }
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,3 +1,20 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View``
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 20px;
+  font-weight: bold;
+  align-self: center;
+  margin-top: 30px;
+`
+
+export const Appointments = styled.FlatList.attrs({
+  contentContainerStyle: {
+    padding: 30,
+    showsVerticalScrollIndicator: false,
+  },
+})``
