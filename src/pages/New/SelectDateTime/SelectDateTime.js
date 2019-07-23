0ae5d5280aac8c8adbfa87f884e84cf2189790d3
Gabriel Monteiro Fernandes <gamofe@protonmail.ch>
2019-07-23 11:46:10 -0300

    Módulo 10 - Selecionando horário

--- src/pages/New/SelectDateTime/SelectDateTime.js
+++ src/pages/New/SelectDateTime/SelectDateTime.js
@@ -1,15 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../../../services/api'
 import Background from '../../../componenents/Background'
 import DateInput from '../../../componenents/DateInput'
-import { Container } from './styles'
+import { Container, Hours, Hour, Title } from './styles'
 
-function SelectDateTime() {
+function SelectDateTime({ navigation }) {
+  const provider = navigation.getParam('provider')
   const [date, setDate] = useState(new Date())
+  const [hours, setHours] = useState([])
+
+  useEffect(() => {
+    async function loadHours() {
+      const { data } = await api.get(`/providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      })
+
+      setHours(data.available)
+    }
+
+    loadHours()
+  }, [date, provider.id])
+
+  function handleSelectHour(hour) {
+    navigation.navigate('Confirm', {
+      provider,
+      hour,
+    })
+  }
 
   return (
     <Background>
       <Container>
         <DateInput date={date} onChange={setDate} />
+        <Hours
+          data={hours}
+          keyExtractor={item => item.hour}
+          renderItem={({ item }) => (
+            <Hour
+              onPress={() => handleSelectHour(item.value)}
+              enabled={item.available}
+            >
+              <Title>{item.hour}</Title>
+            </Hour>
+          )}
+        />
       </Container>
     </Background>
   )
--- src/pages/New/SelectDateTime/styles.js
+++ src/pages/New/SelectDateTime/styles.js
@@ -3,3 +3,25 @@ import styled from 'styled-components/native'
 export const Container = styled.SafeAreaView`
   flex: 1;
 `
+
+export const Hours = styled.FlatList.attrs({
+  numColumns: 2,
+  showsVerticalScrollIndicator: false,
+})``
+
+export const Hour = styled.TouchableOpacity.attrs(props => ({
+  disabled: !props.enabled,
+}))`
+  background: #fff;
+  padding: 20px;
+  flex: 1;
+  opacity: ${props => (props.enabled ? 1 : 0.7)};
+  align-items: center;
+  margin: 0 10px 20px;
+`
+
+export const Title = styled.Text`
+  font-weight: bold;
+  font-size: 14px;
+  color: #333;
+`
